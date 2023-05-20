bce7256df69145ee2dda634a3a3c243e96bc8cd4
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-05-21 00:01:51 +1000

    done task03

---
+++ Task03/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, которая будет
+// выдавать дни недели по заданному номеру
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num >= 1 && num <=7)
+{
+ if(num ==1)
+ {
+Console.WriteLine("Понедельник");
+ }
+if(num ==2)
+ {
+Console.WriteLine("Вторник");
+ }
+if(num ==3)
+ {
+Console.WriteLine("Среда");
+ }
+if(num ==4)
+ {
+Console.WriteLine("Четверг");
+ }
+if(num ==5)
+ {
+Console.WriteLine("Пятница");
+ }
+if(num ==6)
+ {
+Console.WriteLine("Суббота");
+ }
+if(num ==7)
+ {
+Console.WriteLine("Воскресенье");
+ }
+}
+
+else
+{
+    Console.WriteLine("Введено неверное число");
+}
+
+
+
+
+
