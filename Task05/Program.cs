c8b8f3083e4ed876aa7746c7f4a47e1dee434107
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-05-22 10:07:20 +1000

    done task05

---
+++ Task05/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напмшмте программу, которая на вход принимает одно
+// целое число (N), а на выходе показывает все
+// целые числа в промежутке от -N до N.
+
+Console.WriteLine("введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = -number;
+while(i <= number)
+{
+    Console.WriteLine(i);
+    ++i;
+}
