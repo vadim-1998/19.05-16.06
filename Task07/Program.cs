aebda0eba225a677ba72fcf2a4c1e08f5f4b37e8
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-05-22 10:34:23 +1000

    done task07

---
+++ Task07/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход
+// трехзначное число и на выходе показывает последнюю
+// цифру этого числа
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Введите целое трехзначное  число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if( number >= 100 && number <= 999)
+{
+int lastDigit = number % 10;
+Console.WriteLine($"{lastDigit} является последней цифрой числа {number}");
+}
+else
+{
+    Console.WriteLine("Неверное число");
+}
