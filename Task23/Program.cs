e63b9a79d5f2bbd276ea385957133fed30c5f7d4
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-05-26 20:20:05 +1000

    done task23

---
+++ Task23/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+
+Console.WriteLine("Введите натуральное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+TriangleTable(num);
+
+void TriangleTable(int n)
+{
+    int count = 1;
+    while (count <= n)
+    {
+        Console.WriteLine($"{count,3 } -> {count*count*count, 4}");
+        count++;
+  }
+}
