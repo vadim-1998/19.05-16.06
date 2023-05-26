a24b33ca3cdc9262f6972fc708ce3074a4840f8d
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-05-26 18:48:33 +1000

    done task20

---
+++ Task20/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 20: Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,
+
+
+double DistanceFromCoordinates(int ax, int ay, int bx, int by)
+{
+    double sumCathetus = (bx-ax)*(bx-ax) + (by-ay)*(by-ay);
+    double d = Math.Sqrt(sumCathetus);
+    return d;
+}
+
+Console.WriteLine("Введите координату х первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y первой точки");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату х второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату y второй точки");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double result = DistanceFromCoordinates(x1, y1, x2, y2);
+double resultRound = Math.Round(result, 2, MidpointRounding.ToZero);
+Console.WriteLine(resultRound);
