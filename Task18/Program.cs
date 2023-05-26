91614507dc177e0808b17ca71acfadb6f5d5d9ee
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-05-26 20:11:34 +1000

    correct task18

--- Task18/Program.cs
+++ Task18/Program.cs
@@ -3,24 +3,20 @@
 // возможных координат точек в этой четверти (x и y).
 
 
-Console.WriteLine("введите номер четверти");
+Console.WriteLine("Введите номер четверти");
 int quarter = Convert.ToInt32(Console.ReadLine());
-
-if (quarter < 1 || quarter > 4)
+if (quarter < 1 || quarter > 4) 
 {
-    Console.WriteLine("неккоректное число");
+    Console.WriteLine("Некорректно задан номер четверти");
     return;
 }
-string result = Coordinate(quarter);
-Console.WriteLine($" четверть под номером {quarter} соответсвует координатам {result}");
-
-
-string Coordinate(int q)
-
+string result = Coordinates(quarter);
+Console.WriteLine($"Четверти под номером {quarter} соответствуют координаты {result}");
+string Coordinates(int q)
 {
-    if (q == 1) return "x > 0 && y > 0";
-    if (q == 2) return "x < 0 && y > 0";
-    if (q == 3) return "x < 0 && y < 0";
-    if (q == 4) return "x > 0 && y < 0";
+    if (q == 1) return "x > 0, y > 0";
+    if (q == 2) return "x < 0, y > 0";
+    if (q == 3) return "x < 0, y < 0";
+    if (q == 4) return "x > 0, y < 0";
     return null;
 }
