da44fcdde18262d75aa76ece87f70968b14f8b94
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-06-10 19:45:08 +1000

    done task51

---
+++ Task51/Program.cs
@@ -0,0 +1,54 @@
+﻿// задайте двумерный массив
+// найдите сумму элементов находящихся
+// на главной диагонали
+
+int[,] CreateMatrixRndInt(int rows, int columns, int max, int min)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int GetDiagonSum(int[,] matrix)
+{
+    int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                sum += matrix[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 100, -100);
+PrintMatrix(array2d);
+Console.WriteLine(GetDiagonSum(array2d));
