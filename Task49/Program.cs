187c3f80a50071da742eca573112f0d3f527eb2e
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-06-10 19:44:43 +1000

    done task49

---
+++ Task49/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив
+// найдите элементы у которых оба индекса четные
+// и замените эти элементы на их квадраты
+
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int max, int min)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return matrix;
+
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void ReplaceSquadNum(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+
+}
+
+
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 100, -100);
+PrintMatrix(array2d);
+ReplaceSquadNum(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
