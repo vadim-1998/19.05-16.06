a875b8b9dc248bbd0f439b7dbd18a95d6cb390d2
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-06-13 19:29:25 +1000

    done task55

---
+++ Task55/Program.cs
@@ -0,0 +1,67 @@
+﻿
+
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],6}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+void SwapRowsColumns(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0)- 1; i++)
+    {
+        for (int j = i + 1; j < arr.GetLength(1); j++)
+        {
+            int temp = arr[i, j];
+            arr[i, j] = arr[j, i];
+            arr[j, i] = temp;
+        }
+    }
+}
+
+
+bool IsMatrixSquare(int[,] matrix)
+{
+    return matrix.GetLength(0) == matrix.GetLength(1);
+}
+
+
+
+int[,] matr = CreateMatrixRndInt(4, 4, 0, 10);
+PrintMatrix(matr);
+Console.WriteLine();
+
+if(IsMatrixSquare(matr))
+{
+    SwapRowsColumns(matr);
+    PrintMatrix(matr);
+}
+else
+{
+    Console.WriteLine("Matrix isn't square!");
+}
