9bf7e66f57614cebeb7b4f8e3c8cea8965521dfe
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-06-13 19:29:51 +1000

    done task57

---
+++ Task57/Program.cs
@@ -0,0 +1,107 @@
+﻿// Задача 57: Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных.
+
+
+
+int[,] CreateFillMatrix(int m, int n)
+{
+    int[,] arr = new int[m, n];
+    var rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],4}");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int[] FromMatrixToArray(int[,] matr)
+{
+    int[] arr = new int[matr.GetLength(0) * matr.GetLength(1)];
+    int index = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            arr[index++] = matr[i, j];
+        }
+    }
+    return arr;
+}
+
+void CountElements(int[] arr)
+{
+    int currentValue = arr[0];
+    int count = 1;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (currentValue == arr[i])
+        {
+            count++;
+        }
+        else
+        {
+            Console.WriteLine($"There are {count} elements {currentValue}");
+            currentValue = arr[i];
+            count = 1;
+        }
+    }
+    Console.WriteLine($"There are {count} elements {currentValue}");
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write(item + " ");
+    }
+}
+
+int[,] matrix = CreateFillMatrix(5, 5);
+PrintMatrix(matrix);
+Console.WriteLine();
+
+int[] array = FromMatrixToArray(matrix);
+Array.Sort(array);
+PrintArray(array);
+Console.WriteLine();
+CountElements(array);
+
+
+
+
+// int[,,] CreateMatrixRndInt(int rows, int columns, int depth, int min, int max) // rows = 3, columns = 4
+// {
+//     //                        0      1      
+//     int[,,] matrix = new int[rows, columns, depth];
+//     Random rnd = new Random();
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < matrix.GetLength(2); k++)
+//             {
+//                 matrix[i, j, k] = rnd.Next(min, max + 1);
+//             }
+//         }
+//     }
+//     return matrix;
+// }
