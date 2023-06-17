81fc0a822ce038f7021006c343aa65ceb6cc4a71
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-06-17 20:10:38 +1000

    done task67

---
+++ Task67/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+Console.WriteLine("введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num)
+{
+    if( num == 0) return 0;
+     return num % 10 + SumDigits(num / 10);
+
+
+}
+
+
+int sumDigits = SumDigits(number);
+Console.WriteLine($" сумма цифр числа {number} -> {sumDigits}");
