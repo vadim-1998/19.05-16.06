47b41a21d94de83817367d7406ac15ad3f070be7
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-06-17 20:19:30 +1000

    done task69

---
+++ Task69/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите натуральное число");
+int exp = Convert.ToInt32(Console.ReadLine());
+
+
+int Exponentiation(int num, int exp)
+{
+    if( exp == 0) return 1;
+     return num * Exponentiation(num, exp - 1);
+
+
+}
+
+
+
+Console.WriteLine($"  {exp} -> {number} = {Exponentiation(number, exp)}");
